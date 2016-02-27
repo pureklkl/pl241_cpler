4d7fd7af887faca7e4b2ece0aa9dc1287e6735ca
pengyuanfan <pengyuanfan@pengyuanfan-PC>
2016-02-26 16:00:39 -0800

    add function parameter

---
+++ pl241_cpler/src/pl241_cpler/ir/Parameter.java
@@ -0,0 +1,27 @@
+package pl241_cpler.ir;
+
+import pl241_cpler.ir.VariableSet.function;
+
+public class Parameter implements Operand {
+
+	public Parameter(function func, int index){
+		func_ = func;
+		index_ = index;
+	}
+	
+	@Override
+	public int getType() {
+		// TODO Auto-generated method stub
+		return opParam;
+	}
+
+	@Override
+	public String print() {
+		// TODO Auto-generated method stub
+		return new String(func_.print()+": par "+Integer.toBinaryString(index_));
+	}
+
+	private function func_;
+	private int index_;
+	private static final int opParam	=	6;
+}
